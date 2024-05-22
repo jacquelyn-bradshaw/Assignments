099613e8dfde0a36602c610ad6b38df720993b59
jacquelyn-bradshaw <jacquelyn_bradshaw@live.co.uk>
2024-05-22 14:47:15 +0100

    javascript started to control the game

--- assignment-2-javascript/code.js
+++ assignment-2-javascript/code.js
@@ -15,4 +15,62 @@ function saveName(event) {
 
   const gameButtons = document.getElementById("gameButtons")
   gameButtons.style.display = "flex"
+
+  displayWordToBeRead()
 }
+
+function displayWordToBeRead () {
+
+  wordToBeReadElement.replaceChildren()
+
+  const array = wordsArray[wordNumber].word.split("")
+
+  for (i=0; i < array.length; i++) {
+    const listElement = document.createElement("li")
+    listElement.textContent = array[i]
+    wordToBeReadElement.appendChild(listElement)
+  }
+
+  console.log(wordNumber)
+
+  if (wordNumber+1 !== wordsArray.length) {
+    wordNumber ++
+  } else {
+    congratulationsMessage()
+  }
+
+}
+
+function answerClicked(event) {
+  // How to find out which button was clicked
+  const buttonClicked = event.target.dataset.id
+  console.log(buttonClicked)
+
+  if (buttonClicked === wordsArray[0].real) {
+    console.log("Your correct")
+  } else {
+    console.log("Try again")
+  }
+
+  displayWordToBeRead()
+}
+
+function congratulationsMessage() {
+  const paragraphElement = document.createElement("p")
+  paragraphElement.textContent = "Well done"
+  const congratulationsMessageElement = document.getElementById("congratulationsMessage")
+  congratulationsMessageElement.appendChild(paragraphElement)
+}
+
+wordNumber = 0;
+
+const wordToBeReadElement = document.getElementById("wordToBeRead")
+
+const wordsArray = [
+  {word: "grain", real: "yes"},
+  {word: "mub", real: "no"},
+  {word: "trees", real: "yes"},
+  {word: "seed", real: "yes"},
+  {word: "grass", real: "yes"},
+  {word: "cab", real: "yes"}
+]
