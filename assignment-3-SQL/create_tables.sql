c8784caae53b72c676b3fbbe66abd4d05b502b36
jacquelyn-bradshaw <jacquelyn_bradshaw@live.co.uk>
2024-06-06 13:11:28 +0100

    sql code to create my tables

--- assignment-3-SQL/create_tables.sql
+++ assignment-3-SQL/create_tables.sql
@@ -1,19 +1,22 @@
 USE princesses;
 
-CREATE TABLE films
-(film_id VARCHAR(5) PRIMARY KEY,
-name VARCHAR(50) NOT NULL,
-last_watched DATE);
-
 CREATE TABLE princesses
 (princess_id INTEGER AUTO_INCREMENT PRIMARY KEY,
 name VARCHAR(50) NOT NULL UNIQUE,
-film_id VARCHAR(5) NOT NULL,
 age INTEGER CHECK(age > 0),
 first_home VARCHAR(50) NOT NULL,
 second_home VARCHAR(50),
-song VARCHAR(50),
-FOREIGN KEY(film_id) REFERENCES films(film_id)
+song VARCHAR(50)
+);
+
+CREATE TABLE films
+(film_id VARCHAR(5) PRIMARY KEY,
+name VARCHAR(50) NOT NULL,
+princess_id INTEGER NOT NULL,
+year_released INTEGER NOT NULL,
+year_rereleased INTEGER,
+last_watched DATE,
+FOREIGN KEY(princess_id) REFERENCES princesses(princess_id)
 );
 
 CREATE TABLE princesses_owned
