1bfe20e91b6285290a4403796636ac76bac64e62
jacquelyn-bradshaw <jacquelyn_bradshaw@live.co.uk>
2024-06-06 10:19:33 +0100

    sql code to create my tables

--- assignment-3-SQL/create_tables.sql
+++ assignment-3-SQL/create_tables.sql
@@ -10,8 +10,8 @@ CREATE TABLE princesses
 name VARCHAR(50) NOT NULL UNIQUE,
 film_id VARCHAR(5) NOT NULL,
 age INTEGER CHECK(age > 0),
-1st_home VARCHAR(50) NOT NULL,
-2nd_home VARCHAR(50),
+first_home VARCHAR(50) NOT NULL,
+second_home VARCHAR(50),
 song VARCHAR(50),
 FOREIGN KEY(film_id) REFERENCES films(film_id)
 );
