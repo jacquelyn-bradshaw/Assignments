3b7fc5660d50fd8fdcaac8b3ecf03e69bd4f0ad6
jacquelyn-bradshaw <jacquelyn_bradshaw@live.co.uk>
2024-06-26 13:54:58 +0100

    error handling and validation improved

--- assignment-4-APIs/index.js
+++ assignment-4-APIs/index.js
@@ -1,120 +1,141 @@
 const express = require("express")
-const mysql = require("mysql2")
-const path = require('path')
-require('dotenv').config()
+const pool = require("./pool")
+const path = require("path")
+require("dotenv").config()
+
 const app = express()
-const PORT = 3000
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}))
+
+// this enables the use of static files e.g CSS file
 app.use(express.static("assignment-4-APIs/public"))
 
-const pool = mysql.createPool ({
-  host: process.env.MYSQL_HOST,
-  user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD,
-  database: process.env.MYSQL_DATABASE,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0
-})
 
-const filePathIndex = path.resolve(__dirname, 'index.html');
+
+// creates a path to a file
+const filePathIndex = path.resolve(__dirname, "index.html");
 
 app.get("/", (req, res) => {
-  res.sendFile(filePathIndex)
+  try {
+    res.status(200).sendFile(filePathIndex)
+  } catch (error) {
+    console.error("Error sending file", error.message)
+    res.status(500).json({error: "Internal Server Error"})
+  }
 })
 
-app.get("/users", (req, res) => {
-  const sql = "SELECT * FROM users"
-
-  pool.query(sql, (error, results) => {
-    if (error) {
-      console.error("Error fetching users:", error.message)
-      return res.status(500).json({error: "Database error"})
-    }
+app.get("/users", async(req, res) => {
+  try {
+    const sql = `
+      SELECT
+        *
+      FROM
+        users
+    `
+    const [results] = await pool.query(sql)
     res.status(200).json(results)
-  })
+  } catch (error) {
+    console.error("Error fetching users:", error.message)
+    res.status(500).json({error: "Database error"})
+  }
 })
 
-app.get("/scoreboard", (req, res) => {
-  const sql = `
-    SELECT
-      u.name,
-      s.score
-    FROM
-      scores s
-      INNER JOIN users u ON s.user_id = u.user_id
-  `
-
-  pool.query(sql, (error, results) => {
-    if (error) {
-      console.error("Error fetching scores:", error.message)
-      return res.status(500).json({error: "Database error"})
-    }
+app.get("/scoreboard", async(req, res) => {
+  try {
+    const sql = `
+      SELECT
+        u.name,
+        s.score
+      FROM
+        scores s
+        INNER JOIN users u ON s.user_id = u.user_id
+    `
+    const [results] = await pool.query(sql)
     res.status(200).json(results)
-  })
+  } catch (error) {
+    console.error("Error fetching score:", error.message)
+    res.status(500).json({error: "Database error"})
+  }
 })
 
-app.post("/saveuser", (req, res) => {
+app.post("/users", async(req, res) => {
   const {name} = req.body
 
-  const sql = "INSERT INTO users (name) VALUES (?)"
-  pool.query(sql, [name], (error, result) => {
-    if (error) {
-      console.log("Error inserting user:", error.message)
-      return res.status(500).json({error : "Database error"})
+  if (!name) {
+    console.log("Missing name")
+    return res.status(400).json({message: "Please include a name"})
+  }
+
+  try {
+    const findUser = "SELECT user_id FROM users WHERE name = ?"
+    const [findUserResult] = await pool.query(findUser, [name])
+    
+    if (findUserResult.length !== 0) {
+      console.log("User already exists")
+      return res.status(400).json({error: "User already exists"})
     }
-    res.status(201).json({id: result.insertId, message: `User ${name} created successfully`})
-  })
+
+    const sql = "INSERT INTO users (name) VALUES (?)"
+    const [results] = await pool.query(sql, [name])
+    res.status(201).json({id: results.insertId, message: `User ${name} created successfully`})
+  } catch (error) {
+    console.log("Error inserting user:", error.message)
+    res.status(500).json({error : "Database error"})
+  }
 })
 
-app.post("/savescore", async (req, res) => {
+app.post("/scoreboard", async(req, res) => {
   const {name, score} = req.body
-  let userId
 
-  const findUser = "SELECT user_id FROM users WHERE name = ?"
-  userId = await new Promise((resolve, reject) => {
-    pool.query(findUser, [name], (error, result) => {
-      if(error) {
-        console.log("Error finding user:", error.message)
-        return res.status(500).json({error : "Database error"})
-      }
-      
-      if (result.length == 0) {
-        console.log("Error finding user")
-        return res.status(404).json({error : "User not found"})
-      }
-      resolve(result[0].user_id)
-    }) 
-  })
+  if (!name || !score) {
+    console.log("Missing name or score")
+    return res.status(400).json({message: "Please include a name and score"})
+  }
 
-  const sql = "INSERT INTO scores (user_id, score) VALUES (?, ?)"
-  pool.query(sql, [userId, score], (error, result) => {
-    if (error) {
-      console.log("Error inserting score:", error.message)
-      return res.status(500).json({error : "Database error"})
+  try {
+    const findUser = "SELECT user_id FROM users WHERE name = ?"
+    const [findUserResult] = await pool.query(findUser, [name])
+    
+    if (findUserResult.length === 0) {
+      console.log("Error finding user")
+      return res.status(404).json({error: "User not found"})
     }
-    res.status(201).json({id: result.insertId, message: `Score: ${score} for ${name} added successfully`})
-  })
+
+    const userId = findUserResult[0].user_id
+
+    const sql = "INSERT INTO scores (user_id, score) VALUES (?, ?)"
+    const [results] = await pool.query(sql, [userId, score])
+
+    res.status(201).json({id: results.insertId, message: `A score of ${score} for ${name} has been added successfully`})
+  } catch (error) {
+    console.log("Error inserting scores:", error.message)
+    res.status(500).json({error: "Database error"})
+  }
 })
 
-app.delete("/delete-score/:id", (req, res) => {
+app.delete("/scoreboard/:id", async(req, res) => {
   const scoreId = req.params.id
 
-  const sql = "DELETE FROM scores WHERE score_id = ?"
-  pool.query(sql, [scoreId], (error, result) => {
-    if (error) {
-      console.log("Error deleting score:", error.message)
-      return res.status(500).json({error: "Database error"})
-    }
+  if (isNaN(scoreId)) {
+    console.log("score_id is not a number")
+    return res.status(400).json({message: "Please include a number for the score_id in the path"})
+  }
 
-    if (result.affectedRows === 0) {
+  try {
+    const sql = "DELETE FROM scores WHERE score_id = ?"
+    const [results] = await pool.query(sql, [scoreId])
+
+    if (results.affectedRows === 0) {
+      console.log("Error finding score")
       return res.status(404).json({error: "Score not found"})
     }
 
     res.status(200).json({message: "Score deleted successfully"})
-  })
+  } catch (error) {
+    console.log("Error deleting score:", error.message)
+    res.status(500).json({error: "Database error"})
+  }
 })
 
 app.use((error, req, res, next) => {
@@ -122,6 +143,6 @@ app.use((error, req, res, next) => {
   res.status(500).json({error: "Internal Server Error"})
 })
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`)
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on port ${process.env.PORT}`)
 })
